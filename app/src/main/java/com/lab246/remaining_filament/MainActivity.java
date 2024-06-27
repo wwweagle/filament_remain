db538eaab3006eeb4b92add2e7c0572aa27e751a
wwweagle <3746560+wwweagle@users.noreply.github.com>
2024-06-27 23:07:02 +0800

    logic implementation

--- app/src/main/java/com/lab246/remaining_filament/MainActivity.java
+++ app/src/main/java/com/lab246/remaining_filament/MainActivity.java
@@ -4,12 +4,15 @@ import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -31,6 +34,10 @@ public class MainActivity extends AppCompatActivity {
 
         EditText txt_core_diameter = findViewById(R.id.core_diameter);
         EditText txt_full_radius = findViewById(R.id.full_radius);
+        EditText txt_empty_radius = findViewById(R.id.empty_radius);
+        EditText txt_slot_width = findViewById(R.id.slot_width);
+        EditText txt_filament_diameter = findViewById(R.id.filament_diameter);
+        TextView txt_remaining_filament = findViewById(R.id.txt_remaining_filament);
         // ... other EditTexts
 
         TextWatcher textWatcher = new TextWatcher() {
@@ -42,6 +49,22 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
                 // Called as the text is being changed
+                try {
+                    double core_diameter=Double.parseDouble(txt_core_diameter.getText().toString());
+                    double full_radius=Double.parseDouble(txt_full_radius.getText().toString());
+                    double empty_radius=Double.parseDouble(txt_empty_radius.getText().toString());
+                    double slot_width=Double.parseDouble(txt_slot_width.getText().toString());
+                    double filament_diameter=Double.parseDouble(txt_filament_diameter.getText().toString());
+
+                    double result=calc(core_diameter, full_radius, empty_radius, slot_width, filament_diameter);
+                    DecimalFormat decimalFormat = new DecimalFormat("#,##0.00");// Example format
+                    String formattedNumber = decimalFormat.format(result);
+
+                    txt_remaining_filament.setText(formattedNumber);
+
+                }catch(NumberFormatException e){
+
+                }
             }
 
             @Override
@@ -52,11 +75,23 @@ public class MainActivity extends AppCompatActivity {
 
         txt_core_diameter.addTextChangedListener(textWatcher);
         txt_full_radius.addTextChangedListener(textWatcher);
+        txt_empty_radius.addTextChangedListener(textWatcher);
+        txt_slot_width.addTextChangedListener(textWatcher);
+        txt_full_radius.addTextChangedListener(textWatcher);
+        txt_filament_diameter.addTextChangedListener(textWatcher);
         // ... add the listener to other EditTexts
     }
 
 
-    private double calc(){
-        return 0;
+    private double calc(double core_diameter, double full_radius, double empty_radius, double slot_width, double filament_diameter){
+        double avg_cyc_len=((core_diameter+full_radius-empty_radius)*3.14159);
+        double layers=Math.floor((full_radius-empty_radius)/filament_diameter);
+        double cyc_per_layer=Math.floor(slot_width/filament_diameter);
+        double result=avg_cyc_len*layers*cyc_per_layer/1000;
+        if (result<0) {
+            return 0;
+        }else{
+            return result;
+        }
     }
 }
