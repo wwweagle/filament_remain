c4ff163a47a23a45918d1cf0a0eccd4e03a9b3a6
wwweagle <3746560+wwweagle@users.noreply.github.com>
2025-03-09 22:57:31 +0800

    Data persistence

--- app/src/main/java/com/lab246/remaining_filament/MainActivity.java
+++ app/src/main/java/com/lab246/remaining_filament/MainActivity.java
@@ -1,7 +1,9 @@
 package com.lab246.remaining_filament;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.Editable;
+import android.text.InputType;
 import android.text.TextWatcher;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -16,53 +18,15 @@ import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
 
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        EdgeToEdge.enable(this);
-//        setContentView(R.layout.activity_main);
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-//            return insets;
-//        });
-//    }
+    private EditText txtCoreDiameter;
+    private EditText txtFullRadius;
+    private EditText txtEmptyRadius;
+    private EditText txtSlotWidth;
+    private EditText txtFilamentDiameter;
+    private TextView txtRemainingFilament;
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        setupTextWatchers();
-    }
-
-
-    private double calc(double core_diameter, double full_radius, double empty_radius, double slot_width, double filament_diameter){
-        double avg_cyc_len=((core_diameter+full_radius-empty_radius)*3.14159);
-        double layers=Math.floor((full_radius-empty_radius)/filament_diameter);
-        double cyc_per_layer=Math.floor(slot_width/filament_diameter);
-        double result=avg_cyc_len*layers*cyc_per_layer/1000;
-        if (result<0) {
-            return 0;
-        }else{
-            return result;
-        }
-    }
-
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-
-        EditText txt_core_diameter = findViewById(R.id.core_diameter);
-        EditText txt_full_radius = findViewById(R.id.full_radius);
-        EditText txt_empty_radius = findViewById(R.id.empty_radius);
-        EditText txt_slot_width = findViewById(R.id.slot_width);
-        EditText txt_filament_diameter = findViewById(R.id.filament_diameter);
-
-        outState.putString("core_diameter", txt_core_diameter.getText().toString());
-        outState.putString("full_radius", txt_full_radius.getText().toString());
-        outState.putString("empty_radius", txt_empty_radius.getText().toString());
-        outState.putString("slot_width", txt_slot_width.getText().toString());
-        outState.putString("filament_diameter", txt_filament_diameter.getText().toString());
-    }
+    private SharedPreferences prefs;
+    private SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -75,89 +39,113 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
-        if (savedInstanceState != null) {
-            EditText txt_core_diameter = findViewById(R.id.core_diameter);
-            EditText txt_full_radius = findViewById(R.id.full_radius);
-            EditText txt_empty_radius = findViewById(R.id.empty_radius);
-            EditText txt_slot_width = findViewById(R.id.slot_width);
-            EditText txt_filament_diameter = findViewById(R.id.filament_diameter);
+        // Initialize views
+        txtCoreDiameter = findViewById(R.id.core_diameter);
+        txtFullRadius = findViewById(R.id.full_radius);
+        txtEmptyRadius = findViewById(R.id.empty_radius);
+        txtSlotWidth = findViewById(R.id.slot_width);
+        txtFilamentDiameter = findViewById(R.id.filament_diameter);
+        txtRemainingFilament = findViewById(R.id.txt_remaining_filament);
 
-            txt_core_diameter.setText(savedInstanceState.getString("core_diameter"));
-            txt_full_radius.setText(savedInstanceState.getString("full_radius"));
-            txt_empty_radius.setText(savedInstanceState.getString("empty_radius"));
-            txt_slot_width.setText(savedInstanceState.getString("slot_width"));
-            txt_filament_diameter.setText(savedInstanceState.getString("filament_diameter"));
-        }
+        // Set input types for decimal numbers
+        txtCoreDiameter.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_DECIMAL);
+        txtFullRadius.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_DECIMAL);
+        txtEmptyRadius.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_DECIMAL);
+        txtSlotWidth.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_DECIMAL);
+        txtFilamentDiameter.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_DECIMAL);
+
+        // Initialize SharedPreferences
+        prefs = getSharedPreferences("MyPrefs", MODE_PRIVATE);
+        editor = prefs.edit();
+
+        // Get initial values with defaults
+        String coreDiameter = prefs.getString("core_diameter", "76");
+        String fullRadius = prefs.getString("full_radius", "62");
+        String emptyRadius = prefs.getString("empty_radius", "20");
+        String slotWidth = prefs.getString("slot_width", "64");
+        String filamentDiameter = prefs.getString("filament_diameter", "1.75");
+
+        // Set initial values to EditTexts
+        txtCoreDiameter.setText(coreDiameter);
+        txtFullRadius.setText(fullRadius);
+        txtEmptyRadius.setText(emptyRadius);
+        txtSlotWidth.setText(slotWidth);
+        txtFilamentDiameter.setText(filamentDiameter);
+
+        // Save initial values to SharedPreferences (even if they are defaults)
+        editor.putString("core_diameter", coreDiameter)
+                .putString("full_radius", fullRadius)
+                .putString("empty_radius", emptyRadius)
+                .putString("slot_width", slotWidth)
+                .putString("filament_diameter", filamentDiameter)
+                .apply();
+
+        // Update remaining filament immediately after setup
+        updateRemainingFilament();
 
         setupTextWatchers();
     }
 
-    @Override
-    protected void onRestoreInstanceState(Bundle savedInstanceState) {
-        super.onRestoreInstanceState(savedInstanceState);
-
-        if (savedInstanceState != null) {
-            EditText txt_core_diameter = findViewById(R.id.core_diameter);
-            EditText txt_full_radius = findViewById(R.id.full_radius);
-            EditText txt_empty_radius = findViewById(R.id.empty_radius);
-            EditText txt_slot_width = findViewById(R.id.slot_width);
-            EditText txt_filament_diameter = findViewById(R.id.filament_diameter);
-
-            txt_core_diameter.setText(savedInstanceState.getString("core_diameter"));
-            txt_full_radius.setText(savedInstanceState.getString("full_radius"));
-            txt_empty_radius.setText(savedInstanceState.getString("empty_radius"));
-            txt_slot_width.setText(savedInstanceState.getString("slot_width"));
-            txt_filament_diameter.setText(savedInstanceState.getString("filament_diameter"));
-        }
+    private double calc(double core_diameter, double full_radius, double empty_radius, double slot_width, double filament_diameter) {
+        double avg_cyc_len = ((core_diameter + full_radius - empty_radius) * Math.PI);
+        double layers = Math.floor((full_radius - empty_radius) / filament_diameter);
+        double cyc_per_layer = Math.floor(slot_width / filament_diameter);
+        double result = avg_cyc_len * layers * cyc_per_layer / 1000;
+        return Math.max(result, 0);
     }
 
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putString("core_diameter", txtCoreDiameter.getText().toString());
+        outState.putString("full_radius", txtFullRadius.getText().toString());
+        outState.putString("empty_radius", txtEmptyRadius.getText().toString());
+        outState.putString("slot_width", txtSlotWidth.getText().toString());
+        outState.putString("filament_diameter", txtFilamentDiameter.getText().toString());
+    }
 
     private void setupTextWatchers() {
-        EditText txt_core_diameter = findViewById(R.id.core_diameter);
-        EditText txt_full_radius = findViewById(R.id.full_radius);
-        EditText txt_empty_radius = findViewById(R.id.empty_radius);
-        EditText txt_slot_width = findViewById(R.id.slot_width);
-        EditText txt_filament_diameter = findViewById(R.id.filament_diameter);
-        TextView txt_remaining_filament = findViewById(R.id.txt_remaining_filament);
-
         TextWatcher textWatcher = new TextWatcher() {
             @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-                // Called before the text is changed
-            }
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-                // Called as the text is being changed
-                try {
-                    double core_diameter = Double.parseDouble(txt_core_diameter.getText().toString());
-                    double full_radius = Double.parseDouble(txt_full_radius.getText().toString());
-                    double empty_radius = Double.parseDouble(txt_empty_radius.getText().toString());
-                    double slot_width = Double.parseDouble(txt_slot_width.getText().toString());
-                    double filament_diameter = Double.parseDouble(txt_filament_diameter.getText().toString());
-
-                    double result = calc(core_diameter, full_radius, empty_radius, slot_width, filament_diameter);
-                    DecimalFormat decimalFormat = new DecimalFormat("#,##0.00"); // Example format
-                    String formattedNumber = decimalFormat.format(result);
-
-                    txt_remaining_filament.setText(formattedNumber);
-                } catch (NumberFormatException e) {
-                    // Handle the exception if needed
-                }
+                updateRemainingFilament();
             }
 
             @Override
             public void afterTextChanged(Editable s) {
-                // Called after the text has changed
+                editor.putString("core_diameter", txtCoreDiameter.getText().toString())
+                        .putString("full_radius", txtFullRadius.getText().toString())
+                        .putString("empty_radius", txtEmptyRadius.getText().toString())
+                        .putString("slot_width", txtSlotWidth.getText().toString())
+                        .putString("filament_diameter", txtFilamentDiameter.getText().toString())
+                        .apply();
             }
         };
 
-        txt_core_diameter.addTextChangedListener(textWatcher);
-        txt_full_radius.addTextChangedListener(textWatcher);
-        txt_empty_radius.addTextChangedListener(textWatcher);
-        txt_slot_width.addTextChangedListener(textWatcher);
-        txt_filament_diameter.addTextChangedListener(textWatcher);
+        txtCoreDiameter.addTextChangedListener(textWatcher);
+        txtFullRadius.addTextChangedListener(textWatcher);
+        txtEmptyRadius.addTextChangedListener(textWatcher);
+        txtSlotWidth.addTextChangedListener(textWatcher);
+        txtFilamentDiameter.addTextChangedListener(textWatcher);
     }
 
+    private void updateRemainingFilament() {
+        try {
+            double core_diameter = Double.parseDouble(txtCoreDiameter.getText().toString());
+            double full_radius = Double.parseDouble(txtFullRadius.getText().toString());
+            double empty_radius = Double.parseDouble(txtEmptyRadius.getText().toString());
+            double slot_width = Double.parseDouble(txtSlotWidth.getText().toString());
+            double filament_diameter = Double.parseDouble(txtFilamentDiameter.getText().toString());
 
+            double result = calc(core_diameter, full_radius, empty_radius, slot_width, filament_diameter);
+            DecimalFormat decimalFormat = new DecimalFormat("#,##0.00");
+            txtRemainingFilament.setText(decimalFormat.format(result));
+        } catch (NumberFormatException e) {
+            txtRemainingFilament.setText("0.00");
+        }
+    }
 }
